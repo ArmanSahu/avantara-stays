5d7b9b9c14425731839f05109d2d0731156537ba
Arman Sahu <armansahu619@gmail.com>
2026-03-27 15:37:31 +0530

    Added the review schema

---
+++ backend/src/models/reviews-model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const {Schema,model} = mongoose;
+const ObjectId = Schema.Types.ObjectId;
+
+const reviewSchema = new Schema({
+    userId: {
+        type: ObjectId,
+        required: true,
+        ref: "users"
+    },
+    propertyId: {
+        type: ObjectId,
+        required: true,
+        ref: "Property"
+    },
+    bookingId:{
+        type: ObjectId,
+        required: true,
+        unique: true,  
+        ref: "Bookings"
+    },
+    rating: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5
+    },
+    review: {
+        type: String
+    } 
+});
+
+reviewSchema.index({propertyId: 1});
+reviewSchema.index({userId:1,propertyId:1,bookingId:1});
+
+const reviewModel = model("Reviews",reviewSchema);
+
+module.exports = reviewModel;
